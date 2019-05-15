53033df24b10f5227f56643dff8e63c5dccecac1
Melissa G <jstmac@gmail.com>
2019-05-14 22:02:35 -0600

    All Pages Working

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'people/index'
-  get 'people/show'
-  get 'people/new'
-  get 'people/edit'
+  
+  resources :people
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
